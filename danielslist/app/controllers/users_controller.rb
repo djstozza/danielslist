4b57f26ab826d42284b65cfd684efbd38ece0554
djstozza <daniel.sztolcman@gmail.com>
2015-09-04 19:53:12 +1000

    fixed login bug

--- danielslist/app/controllers/users_controller.rb
+++ danielslist/app/controllers/users_controller.rb
@@ -9,8 +9,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new user_params
 		if @user.save
-			@user = @current_user
-			redirect_to user_path(params[:id])
+			redirect_to root_path
 		else
 			render :new
 		end
