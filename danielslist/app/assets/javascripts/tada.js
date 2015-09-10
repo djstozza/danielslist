5df518db07c88a052987d80f25049e2557dde6ff
djstozza <daniel.sztolcman@gmail.com>
2015-09-11 03:02:10 +1000

    fixe tiling in item indexes

--- danielslist/app/assets/javascripts/tada.js
+++ danielslist/app/assets/javascripts/tada.js
@@ -1,6 +1,6 @@
 tada = {
 	start: function () {
-		$('h1.heading').hover(function () {
+		$('h1.heading').mouseover(function () {
 		$('h1.heading').addClass('tada')
 		});
 		setTimeout(tada.stop, 2002);
