8230cf0c5c3e14254532d184b0cf77b20e71f895
djstozza <daniel.sztolcman@gmail.com>
2015-09-14 11:06:33 +1000

    use find_by rather than where in conversations_controller since there is only a single recipient rather than a group

--- danielslist/app/controllers/conversations_controller.rb
+++ danielslist/app/controllers/conversations_controller.rb
@@ -3,48 +3,48 @@ class ConversationsController < ApplicationController
 	before_action :conversation, :only => [:show]
 
 	def new
-  	end
+    end
 
-  	def create
-    	recipients = User.where(id: conversation_params)
-    	conversation = @current_user.send_message(conversation_params[:recipients], conversation_params[:body], conversation_params[:subject]).conversation
-    	redirect_to conversation_path(conversation)
-    	flash[:message] = "Your message was successfully sent!"
-  	end
+  def create
+    # raise "why don't you load??"
+    recipients = User.find_by(id: conversation_params[:recipients])
+    conversation = @current_user.send_message(recipients, conversation_params[:body], conversation_params[:subject]).conversation
+    flash[:success] = "Your message was successfully sent!"
+    redirect_to conversation_path(conversation)
+  end
 
-  	def show
-    	@receipts = conversation.receipts_for(@current_user)
-    	# mark conversation as read
-    	conversation.mark_as_read(@current_user)
-  	end
+  def show
+    @receipts = conversation.receipts_for(@current_user)
+    # mark conversation as read
+    conversation.mark_as_read(@current_user)
+  end
 
-  	def reply
-    	@current_user.reply_to_conversation(conversation, message_params[:body])
-    	redirect_to conversation_path(conversation)
-    	flash[:message] = "Your reply message was successfully sent!"
-  	end
+  def reply
+    @current_user.reply_to_conversation(conversation, message_params[:body])
+    flash[:notice] = "Your reply message was successfully sent!"
+    redirect_to conversation_path(conversation)
+  end
 
-  	def trash
-    	conversation.move_to_trash(@current_user)
-    	redirect_to mailbox_inbox_path
-  	end
+  def trash
+      conversation.move_to_trash(@current_user)
+      redirect_to mailbox_inbox_path
+    end
 
-  	def untrash
-    	conversation.untrash(@current_user)
-    	redirect_to mailbox_inbox_path
-  	end
+    def untrash
+      conversation.untrash(@current_user)
+      redirect_to mailbox_inbox_path
+    end
 
+  private
+  def check_if_logged_in
+    redirect_to root_path unless @current_user.present?
+  end
 
-  	private
-  	def check_if_logged_in
-      redirect_to root_path unless @current_user.present?
- 	  end
+  def conversation_params
+      params.require(:conversation).permit(:subject, :body, :recipients)
+    end
 
- 	  def conversation_params
-    	params.require(:conversation).permit(:subject, :body, :recipients => [])
-  	end
-  	
-  	def message_params
-    	params.require(:message).permit(:body, :subject)
-  	end
+    def message_params
+      params.require(:message).permit(:body, :subject)
+    end
 end
