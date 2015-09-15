d71fcc04416543cb96f97995f04309bfc95525d0
djstozza <daniel.sztolcman@gmail.com>
2015-09-15 11:16:17 +1000

    get rid of config.action_mailer.smtp_settings = { port: 25 } since no mailer

--- danielslist/config/initializers/mailboxer.rb
+++ danielslist/config/initializers/mailboxer.rb
@@ -18,5 +18,4 @@ Mailboxer.setup do |config|
   #Configures maximum length of the message subject and body
   config.subject_max_length = 255
   config.body_max_length = 32000
-  config.action_mailer.smtp_settings = { port: 25 }
 end
